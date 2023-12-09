d180caabe9be9e18f31c6130ecb8442bf8585974
ReignBit <bitbytegaming@gmail.com>
2023-12-09 22:02:14 +0000

    Added JSON marshaling

--- src/routes/guild.js
+++ src/routes/guild.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+
 const { Guild } = require("../models");
 
 router.use(require("./middleware/auth"));
@@ -22,12 +23,13 @@ router.get("/:guildId", async(req, res) => {
     let a = await Guild.find({snowflake: req.params.guildId});
     if (a.length)
     {
-        return res.json(
+        res.json(
             {
                 data: a,
                 msg: "guild"
             }
         );
+        return;
     }
 
     return res.status(404).json({
@@ -35,5 +37,35 @@ router.get("/:guildId", async(req, res) => {
         msg: `Guild with snowflake '${req.params.guildId}' not found!`
     });
 });
+
+function marshalBody(req, res) {
+    var clean = {};
+    for (var i in req.body) {
+        if (req.body[i] !== null) {
+            clean[i] = req.body[i];
+        }
+    }
+    return clean;
+}
+
+router.post("/:guildId", async(req, res) =>{
+    console.log(req.body);
+    const clean = marshalBody(req, res);
+
+    let a = await Guild.findOne({snowflake: req.params.guildId});
+    if (a) {
+        console.log(a);
+        return res.json({data: a, msg: "guild updated"});
+    }    
+    
+    try{
+        a = new Guild(clean);
+        a.save();
+        return res.json({data: [a], msg: "guild created"});
+    } catch(e) 
+    {
+        return res.send(500).json({data: [], msg: `Failed to upsert guildid ${req.params.guildId}`});
+    }
+})
 
 module.exports = router;
